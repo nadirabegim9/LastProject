132cd23ab098c0cb855af984d0eba2562ccf7495
HP <kozoarovanadirabegim@gmail.com>
2024-06-09 17:52:41 +0600

    Docker_222

--- LastProject/mysite/admin.py
+++ LastProject/mysite/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import *
+
+admin.site.register(Course)
+admin.site.register(Timetable)
+admin.site.register(Post_course)
+admin.site.register(Homework)
+admin.site.register(Post_homework)
+
+from django.contrib import admin
+from .models import *
 from modeltranslation.admin import TranslationAdmin
 #
 
@@ -61,76 +70,3 @@ class CabinetAdmin(TranslationAdmin):
         }
 
 
-# @admin.register(Homework)
-# class HomeworkAdmin(TranslationAdmin):
-#     list_display = '__all__'
-#
-#     class Media:
-#         js = (
-#             'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-#             'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-#             'modeltranslation/js/tabbed_translation_fields.js',
-#         )
-#         css = {
-#             'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-#         }
-#
-#
-# @admin.register(Course)
-# class ProfessorAdmin(TranslationAdmin):
-#     list_display = ('name')
-#
-#     class Media:
-#         js = (
-#             'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-#             'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-#             'modeltranslation/js/tabbed_translation_fields.js',
-#         )
-#         css = {
-#             'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-#         }
-#
-#
-# @admin.register(Timetable)
-# class ProfessorAdmin(TranslationAdmin):
-#     list_display = ('course',)
-#
-#     class Media:
-#         js = (
-#             'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-#             'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-#             'modeltranslation/js/tabbed_translation_fields.js',
-#         )
-#         css = {
-#             'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-#         }
-#
-#
-# @admin.register(Post_course)
-# class ProfessorAdmin(TranslationAdmin):
-#     list_display = '__all__'
-#
-#     class Media:
-#         js = (
-#             'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-#             'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-#             'modeltranslation/js/tabbed_translation_fields.js',
-#         )
-#         css = {
-#             'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-#         }
-#
-#
-# @admin.register(Post_homework)
-# class ProfessorAdmin(TranslationAdmin):
-#     list_display = '__all__'
-#
-#     class Media:
-#         js = (
-#             'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-#             'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-#             'modeltranslation/js/tabbed_translation_fields.js',
-#         )
-#         css = {
-#             'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-#         }
